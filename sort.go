b2155c490162eb16acb6e2a72f6178ca3ea0b10d
Harkishen-Singh <harkishensingh@hotmail.com>
2023-08-16 18:29:21 +0530

    try fix on ubuntu
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>

--- sort.go
+++ sort.go
@@ -27,7 +27,7 @@ func sortFilesByCreationTime(files []string) ([]string, error) {
 			return nil, fmt.Errorf("failed to cast to syscall.Stat_t")
 		}
 
-		changeTime := time.Unix(int64(statT.Ctimespec.Sec), int64(statT.Ctimespec.Nsec))
+		changeTime := time.Unix(int64(statT.Mtimespec.Sec), int64(statT.Mtimespec.Nsec))
 
 		fileTimes[i] = fileWithTime{name: file, time: changeTime}
 	}
